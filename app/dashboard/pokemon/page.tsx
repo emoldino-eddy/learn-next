3f0e76f41e1b6b4d6ed0f31708a84916470dd6fa
emoldino-eddy <eddy.park@emoldino.com>
2025-05-19 15:33:11 +0900

    make initalData

--- app/dashboard/pokemon/page.tsx
+++ app/dashboard/pokemon/page.tsx
@@ -1,13 +1,41 @@
 import { lusitana } from '@/app/ui/fonts';
 import Pokemon from '@/app/ui/pockmon/card';
 
-export default function Page() {
+export default async function Page() {
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`
+  );
+  const data = await res.json();
+  const pokemonList = data.results;
+  
+  const detailResults = await Promise.all(
+    pokemonList.map(async (pokemon: { name: string; url: string }) => {
+      const details = await fetch(pokemon.url);
+      const detailsData = await details.json();
+      return {
+        id: detailsData.id,
+        name: pokemon.name,
+        image: detailsData.sprites.other['official-artwork'].front_default,
+      };
+    })
+  );
+
+  const initialData = {
+    pages: [{
+      results: detailResults,
+      hasMore: data.next !== null,
+      nextOffset: 20
+    }],
+    pageParams: 0
+  };
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Pockmons</h1>
       </div>
-      <Pokemon />
+      
+      <Pokemon initialData={initialData} />
     </div>
   );
 }
--- app/ui/pockmon/card.tsx
+++ app/ui/pockmon/card.tsx
@@ -5,11 +5,12 @@ import { getAllPokemon } from '@/packages/api/poke-data';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { useEffect, useRef } from 'react';
 
-export default function Pokemon() {
+export default function Pokemon({ initialData }: any) {
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
     useInfiniteQuery({
       queryKey: ['pokemon'],
       queryFn: getAllPokemon,
+      initialData: initialData,
       getNextPageParam: (lastPage) => {
         return lastPage.hasMore ? lastPage.nextOffset : undefined;
       },
@@ -38,25 +39,23 @@ export default function Pokemon() {
 
   return (
     <div className="p-4 grid-cols-2 md:grid-cols-4 gap-4">
-      {data?.pages.map((page, i) => (
-        <div key={i}>
-          {page.results.map((pokemon: { name: string; image: string }) => (
-            <div
-              key={pokemon.name}
-              className="border rounded-xl p-4 flex flex-col items-center shadow-md"
-            >
-              <Image
-                src={pokemon.image}
-                alt={pokemon.name}
-                width={100}
-                height={100}
-                className="w-20 h-20 object-contain mb-2"
-              />
-              <p className="capitalize font-medium">{pokemon.name}</p>
-            </div>
-          ))}
-        </div>
-      ))}
+      {data?.pages.flatMap((page) =>
+        page.results.map((pokemon: { name: string; image: string; id: number }) => (
+          <div
+            key={pokemon.id}
+            className="border rounded-xl p-4 flex flex-col items-center shadow-md"
+          >
+            <Image
+              src={pokemon.image}
+              alt={pokemon.name}
+              width={100}
+              height={100}
+              className="w-20 h-20 object-contain mb-2"
+            />
+            <p className="capitalize font-medium">{pokemon.name}</p>
+          </div>
+        ))
+      )}
       <div ref={observerRef} className="col-span-full py-4 text-center"/>
         
     </div>
